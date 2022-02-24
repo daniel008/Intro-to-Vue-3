7ce3cb317a100fe796b80706c366833aa44077e2
DanielMa <mwldaniel_008@126.com>
2022-02-24 19:46:22 +0800

    merging branch L3-start into main

--- main.js
+++ main.js
@@ -2,12 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-<<<<<<< HEAD
       description: 'A pair of warm and cozy socks',
-=======
       image: './assets/images/socks_green.jpg',
       url: 'https://www.happysocks.com/hk/',
->>>>>>> L3-start
     }
   },
 })
